affe4eb8632c1038004fb60105208fa9ae5942d3
Adiyoga <adiyoga27new@gmail.com>
2023-12-13 17:37:59 +0800

    a

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const qrcode = require('qrcode-terminal');
+const fs = require('fs');
 var request = require('request');
 
 
@@ -6,8 +7,8 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const client = new Client({
     restartOnAuthFail: true,
     puppeteer: {
-        // executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
-        executablePath: '/usr/bin/google-chrome-stable',
+        executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+        // executablePath: '/usr/bin/google-chrome-stable',
         headless: true,
         args: [
           '--no-sandbox',
@@ -28,8 +29,66 @@ client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
 });
 
-client.on('ready', () => {
+client.on('ready',async () => {
     console.log('Client is ready!');
+    const allExtractedData = [];
+    const chats = await client.getChats()
+ 
+// Iterate through each chat
+for (const chat of chats) {
+  console.log(chat);
+  // Fetch messages for the current chat
+  const messages = await chat.fetchMessages({ limit: 100 });
+
+  // Extracted data for each message
+  const extractedData = await Promise.all(messages.map(async (message) => {
+    const extractedMessage = {
+      ack: message.ack,
+      from: message.from,
+      to: message.to,
+      author: message.author,
+      type: message.type,
+      body: message.body,
+      fromMe: message.fromMe,
+      hasMedia: message.hasMedia,
+      timestamp: message.timestamp,
+      deviceType: message.deviceType,
+    };
+
+    if (message.hasMedia) {
+      try {
+        // Download media and add it to the extracted message
+        const mediaFile = await message.downloadMedia();
+        extractedMessage.mediaFile = mediaFile;
+      } catch (error) {
+        console.error('Error downloading media:', error);
+      }
+    }
+
+    return extractedMessage;
+  }));
+
+  // Contact information for the current chat
+  const contactInfo = {
+    id: chat.id,
+    isGroup: chat.isGroup,
+    name: chat.name,
+    onChat: extractedData,
+  };
+
+  // Add the extractedData to the array
+  allExtractedData.push(contactInfo);
+}
+const jsonFilePath = 'allExtractedData.json';
+fs.writeFileSync(jsonFilePath, JSON.stringify(allExtractedData, null, 2));
+
+
+// console.log(allExtractedData);
+
+// Log the extracted data
+
+// console.log(extractedData);
+
 });
 client.on('message', async message => {
 	console.log(message);
